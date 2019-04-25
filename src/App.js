86f350e6b3dc334d89044d353878177ae04c29f9
Abdan Nafik <abdan.nafik@pln.co.id>
2019-04-26 06:52:35 +0700

    latihan 1 react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const coba = 'Belajar React'
+
+const friends = [
+  { id : 1, name : 'Robert'},
+  { id : 2, name : 'Budi'},
+  { id : 3, name : 'Arum'},
+  { id : 4, name : 'Roger'}
+]
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name : 'pln corpu',
+      bestFriends : [
+        { id : 1, name : 'Lola'},
+        { id : 2, name : 'Tomo'},
+        { id : 3, name : 'Brudin'},
+        { id : 4,name : 'Joko'}
+      ]
+    }
+  }
+
+  fiterFriends () {
+    return friends.map(f => {
+      if(f.id %2 === 0) {
+        return (
+          <p> {f.id} - {f.name} </p>
+        )
+      }
+    })
+  }
+
+  render () {
+    return (
+        <div>
+          <h1> Hello { coba } { (this.state.name) }</h1>
+          <button 
+            onClick={ () => this.setState({
+                bestFriends:[]
+              })
+            }>
+          Click Me
+          </button>
+          {
+            this.fiterFriends()
+          }
+
+          {
+            //cara akses data dari constructor
+            this.state.bestFriends.map(f => {
+              return (
+                <p> Best Friends - {f.id} - {f.name} </p>
+              )
+            })
+          }
+
+        </div>
+    )
+  }
+
 }
+
 
 export default App;
